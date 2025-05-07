e66b00360db29999937020e5788518b7f769b1db
ClodsNegreiros <clodsn@gmail.com>
2025-05-06 23:04:34 -0300

    feat: criado primeira migration

---
+++ HeroAgency.Infrastructure/Migrations/20250507004800_InitialCreate.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HeroAgency.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperHeroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    HeroName = table.Column<string>(type: "text", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Height = table.Column<decimal>(type: "numeric", nullable: false),
+                    Weight = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeroes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SuperPowers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Power = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperPowers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SuperHeroPowers",
+                columns: table => new
+                {
+                    SuperPowerId = table.Column<int>(type: "integer", nullable: false),
+                    SuperHeroId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeroPowers", x => new { x.SuperHeroId, x.SuperPowerId });
+                    table.ForeignKey(
+                        name: "FK_SuperHeroPowers_SuperHeroes_SuperHeroId",
+                        column: x => x.SuperHeroId,
+                        principalTable: "SuperHeroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SuperHeroPowers_SuperPowers_SuperPowerId",
+                        column: x => x.SuperPowerId,
+                        principalTable: "SuperPowers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SuperHeroPowers_SuperPowerId",
+                table: "SuperHeroPowers",
+                column: "SuperPowerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHeroPowers");
+
+            migrationBuilder.DropTable(
+                name: "SuperHeroes");
+
+            migrationBuilder.DropTable(
+                name: "SuperPowers");
+        }
+    }
+}

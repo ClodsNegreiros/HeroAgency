87ba3f688d2defa482cccd866ccf32dbd0e712f5
ClodsNegreiros <clodsn@gmail.com>
2025-05-06 23:04:17 -0300

    feat: registrado as entidades SuperHero, SuperPower e SuperHeroPower no DbContext

--- HeroAgency.Infrastructure/Context/ApplicationDbContext.cs
+++ HeroAgency.Infrastructure/Context/ApplicationDbContext.cs
@@ -8,6 +8,10 @@ namespace HeroAgency.Infrastructure.Context
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         { }
 
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+        public DbSet<SuperPower> SuperPowers { get; set; }
+        public DbSet<SuperHeroPower> SuperHeroPowers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);

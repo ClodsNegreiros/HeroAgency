a720dd7ddab714cd59394da6e4e9ecfa289f51b2
ClodsNegreiros <clodsn@gmail.com>
2025-05-07 15:13:57 -0300

    feat: configurado CORS para aplicação na porta 4200

--- HeroAgency/Program.cs
+++ HeroAgency/Program.cs
@@ -17,8 +17,19 @@ namespace HeroAgency
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+
+            // Add CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAngularDevServer",
+                    builder => builder
+                        .WithOrigins("http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -27,8 +38,7 @@ namespace HeroAgency
 
             // DbContext
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString));
-
+                options.UseNpgsql(connectionString));
 
             // Add Mediator DI
             builder.Services.AddMediatR(cfg =>
@@ -58,8 +68,10 @@ namespace HeroAgency
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            // Use CORS
+            app.UseCors("AllowAngularDevServer");
 
+            app.UseAuthorization();
 
             app.MapControllers();
 

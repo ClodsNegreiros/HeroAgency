34f2adb2b2cf7999bdf80ced39b1e9ed840ebb04
ClodsNegreiros <clodsn@gmail.com>
2025-05-06 23:21:49 -0300

    feat: implementado repository para SuperHero com operações CRUD

---
+++ HeroAgency.Domain/Interfaces/ISuperHeroRepository.cs
@@ -0,0 +1,13 @@
+﻿using HeroAgency.Domain.Entities;
+
+namespace HeroAgency.Domain.Interfaces
+{
+    public interface ISuperHeroRepository
+    {
+        Task<SuperHero> CreateAsync(SuperHero superHero);
+        Task<bool> DeleteAsync(SuperHero superHero);
+        Task<List<SuperHero>> GetAll();
+        Task<SuperHero?> GetById(int id);
+        Task<SuperHero> UpdateAsync(SuperHero superHero);
+    }
+}
---
+++ HeroAgency.Infrastructure/Repositories/SuperHeroRepository.cs
@@ -0,0 +1,53 @@
+﻿using HeroAgency.Domain.Entities;
+using HeroAgency.Domain.Interfaces;
+using HeroAgency.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeroAgency.Infrastructure.Repositories
+{
+    public class SuperHeroRepository : ISuperHeroRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SuperHeroRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<SuperHero> CreateAsync(SuperHero superHero)
+        {
+            await _context.SuperHeroes.AddAsync(superHero);
+            await _context.SaveChangesAsync();
+
+            return superHero;
+        }
+
+        public async Task<SuperHero> UpdateAsync(SuperHero superHero)
+        {
+            _context.SuperHeroes.Update(superHero);
+            await _context.SaveChangesAsync();
+
+            return superHero;
+        }
+
+        public async Task<SuperHero?> GetById(int id)
+        {
+            return await _context.SuperHeroes
+                .Where(sh => sh.Id == id)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<List<SuperHero>> GetAll()
+        {
+            return await _context.SuperHeroes.ToListAsync();
+        }
+
+        public async Task<bool> DeleteAsync(SuperHero superHero)
+        {
+            _context.SuperHeroes.Remove(superHero);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}

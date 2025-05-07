faa621b5e039e50a1862ecfba084240c83c88757
ClodsNegreiros <clodsn@gmail.com>
2025-05-06 23:23:26 -0300

    feat: implementado BaseUseCase com MediatR

---
+++ HeroAgency.Application/UseCases/BaseUseCase.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace HeroAgency.Application.UseCases
+{
+    public abstract class BaseUseCase
+    {
+        protected readonly IMediator mediator;
+        protected BaseUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+    }
+}
